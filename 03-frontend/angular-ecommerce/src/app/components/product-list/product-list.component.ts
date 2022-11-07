a09a82385ebfb3a9918313dc5b1e953e89e6f6da
Mirza Suljic <mirza.suljic@abc-techgroup.com>
2022-11-07 21:59:55 +0100

    add product categories

--- 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,23 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ProductService } from 'src/app/services/product.service';
 import { Product } from './../../common/product';
 
 @Component({
   selector: 'app-product-list',
-  // templateUrl: './product-list.component.html',
   templateUrl: './product-list-grid.component.html',
   styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
-  constructor(private productService: ProductService) {}
+  currentCategoryId!: number;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.listProducts();
-  }
-  listProducts() {
-    this.productService.getProductList().subscribe((data) => {
-      this.products = data;
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
     });
+  }
+
+  listProducts() {
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      this.currentCategoryId = 1;
+    }
+
+    this.productService
+      .getProductList(this.currentCategoryId)
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 }
