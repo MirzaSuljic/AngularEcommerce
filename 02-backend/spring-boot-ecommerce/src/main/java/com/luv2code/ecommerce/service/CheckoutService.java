c98143dd0bdf7ac35e32ca60652d9608eae0bdd0
Mirza Suljic <mirza.suljic@abc-techgroup.com>
2022-11-17 13:48:31 +0100

    add purchase save on DB

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutService.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutService.java
@@ -3,6 +3,6 @@ package com.luv2code.ecommerce.service;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 
-public interface CeckoutService {
+public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
 }
