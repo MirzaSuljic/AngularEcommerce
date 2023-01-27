dfe61945d7b5182d939fba20b4a5e89872b4e032
Mirza Suljic <mirza.suljic@abc-techgroup.com>
2023-01-27 08:53:45 +0100

    remove WebSecurityConfigurerAdapter

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/security/SecurityConfig.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/security/SecurityConfig.java
@@ -1,27 +1,43 @@
 package com.luv2code.ecommerce.security;
 
 
+import com.luv2code.ecommerce.filter.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration @EnableWebSecurity @RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig  {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Override
+    @Bean
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       http.csrf().disable();
+       http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+       http.authorizeRequests().anyRequest().permitAll();
+       http.addFilter(new CustomAuthenticationFilter(webSecurityCustomizer()));
+
+       return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer()throws Exception{
+        return  webSecurityCustomizer();
     }
 }
