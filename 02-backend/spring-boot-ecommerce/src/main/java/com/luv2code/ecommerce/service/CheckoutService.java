e69c1205de11b06a56111d8ce447c5ac6ea2c71c
indirkaric <63605171+indirkaric@users.noreply.github.com>
2023-02-07 10:26:12 +0100

    Update CheckoutService.java

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutService.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutService.java
@@ -2,7 +2,7 @@ package com.luv2code.ecommerce.service;
 
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
-
+// create package for implementation   example  lu2code.eccomerce.service.impl and that package will have implementation of interfaces
 public interface CheckoutService {
     PurchaseResponse placeOrder(Purchase purchase);
 }
